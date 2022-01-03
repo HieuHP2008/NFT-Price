18ac818e3e741bde96e537fcf95dd009178f1a61
RollerPhan <hieuxautraifa2017@gmail.com>
2022-01-03 09:24:04 +0700

    add button alert

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 var nft_box = document.querySelector('.nft-box')
-var buy_button = document.querySelector('.button-19')
+var button = document.querySelector('.buy_button')
+
 function random() {
     random_number = Math.floor(Math.random() * 5193)
     if (random_number <= 1000)
         nft_box.innerHTML = `<h1 class='cheap'>Price: ${random_number}$ ⭸</h1>`
     else
         nft_box.innerHTML = `<h1 class='expensive'>Price: ${random_number}$ ⭷</h1>`
-}
-buy_button.onclick = function () {
-    alert("You don't have enought money to buy this NFT")
+    button.onclick = () => {
+        if (random_number <= 1200)
+            alert("This NFT was bought by you")
+        else 
+            alert("You don't have enough money to buy this NFT")
+    }
 }
 setInterval(random,5000)
